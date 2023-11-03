e80574dd44d87ad1c603d4d35659c5608dde9bef
Lbirlouet <lucasbirlouet@gmail.com>
2023-11-03 18:35:24 +0100

    ok dans les grandes lignes, faut gerer leaks mnt

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: lbirloue <lbirloue@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 17:38:44 by lbirloue          #+#    #+#             */
-/*   Updated: 2023/11/03 17:22:33 by lbirloue         ###   ########.fr       */
+/*   Updated: 2023/11/03 18:34:39 by lbirloue         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,7 @@ char	*gnl2(int fd, char *tempo)
 	str = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!str)
 		return (NULL);
-	while (i != 0 || ft_strchr(tempo, '\n') == 0)
+	while (i != 0 && ft_strchr(tempo, '\n') == 0)
 	{
 		i = read(fd, str, BUFFER_SIZE);
 		if (i == -1)
@@ -39,11 +39,11 @@ char	*next_line(char *tempo)
 {
 	char *ret;
 	int i;
-	
+
 	i = 0;
 	while (tempo[i] != '\n' && tempo[i] != 0)
 		i++;
-	ret = malloc(sizeof(char) * i + 2);
+	ret = malloc(sizeof(char) * (i + 2));
 	if (!ret)
 		return (NULL);
 	i = 0;
@@ -63,20 +63,20 @@ char	*refresh(char *tempo)
 	char *ret;
 	int i;
 	int j;
-	
+
 	i = 0;
 	j = 0;
 	while (tempo[i] != '\n' && tempo[i] != 0)
 		i++;
 	if (tempo[i] == 0)
 		return (NULL);
-	ret = malloc(sizeof(char) * ft_strlen(tempo) + 2);
-	while (tempo[i] != 0 && tempo[i] != '\n')
+	if (tempo[i] == '\n')
+		i ++;
+	ret = malloc(sizeof(char) * (ft_strlen(tempo) - i + 1));
+	while (tempo[i] != 0)
 	{
 		ret[j++] = tempo[i++];
 	}
-	if (tempo[i] == '\n')
-		ret[j++] = '\n';
 	ret[j] = 0;
 	return (ret);
 }
@@ -95,26 +95,17 @@ char	*get_next_line(int fd)
 }
 
 
-
-
-
-
-
-
-
-
-
  #include <fcntl.h>
 int main()
 {
-	int i = 6;
+	int i = 93;
 	char	*line = NULL;
 	int fd = open("test", O_RDONLY, 0644);
 	
 	if (fd == -1)
 		return (1);
 	line = get_next_line(fd);
-	printf("\n|%s|\n", line);
+	printf("%s", line);
 	while (i > 0)
 	{
 		free(line);
